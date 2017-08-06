b6e9fda0d3570e2099b40fff7e3c3c52f101b644
Joshua Ugba <justjosh001@gmail.com>
2017-08-06 05:56:14 +0100

    Uses nodemailer to send mails send from contact form

--- app.js
+++ app.js
@@ -18,5 +18,34 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
   res.render("about");
 });
+app.get('/contact', function(req, res) {
+  res.render("contact");
+});
+app.post('/contact/send', function(req, res) {
+  let tranporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: '',
+      pass: ''
+    }
+  });
+  let mailOptions = {
+    from: 'Josh <justjosh001@gmail.com>',
+    to: 'joshua.ugba@andela.com',
+    subject: 'Website Submission',
+    text: 'You have a Submission with the following details..Name: '+req.body.name+' Email: '+req.body.email + ' Message: ' +req.body.message,
+    html: '<p>You have a Submission with the following details..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email + '</li><li> Message: '+ req.body.message+'</li></ul>'
+  }
+  tranporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.redirect('/');
+    }else {
+      console.log('Message Sent: ' + info.response);
+      res.redirect('/');
+    }
+  })
+});
+
 app.listen(3000)
 console.log('Server is running at 3000.....');
