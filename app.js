bb8613d8963ca16b990a9ddbc1c3d9d2c2fd7edd
Joshua Ugba <justjosh001@gmail.com>
2017-08-06 04:27:58 +0100

    Adds pug to render views

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-  res.send("Hello world");
+  res.render("index");
 });
 app.listen(3000)
 console.log('Server is running at 3000.....');
